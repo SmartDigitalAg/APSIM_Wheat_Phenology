384b7eea2359784fe5e3b3a08a9ee84432a283de
EthanSeok <champ9162@naver.com>
2024-08-07 19:47:05 +0900

    Divide Wheat Stages: 코드 간결화

--- wheat_stage.py
+++ wheat_stage.py
@@ -3,205 +3,46 @@ from datetime import datetime
 from thermal_time import APSIMWheatPhenology
 import os
 
-def emerg2endjuv(df, stage_div):
-    juvenile = df.dropna(subset=['Emergence_date'])
-    first_emergence_index = juvenile['Emergence_date'].first_valid_index()
-
-    if first_emergence_index is not None:
-        juvenile = df.loc[first_emergence_index + 1:].copy()
-        juvenile['juvenile_tt'] = juvenile['delta_TT'].cumsum()
-
-        tt_emergence = stage_div['tt_emergence']
-        emergence_exceedance = juvenile[juvenile['juvenile_tt'] >= tt_emergence]
-
-        if not emergence_exceedance.empty:
-            first_exceedance_index = emergence_exceedance.index[0]
-            previous_day_index = first_exceedance_index - 1 if first_exceedance_index > juvenile.index[0] else first_exceedance_index
-
-            prev_day_tt = juvenile.loc[previous_day_index, 'juvenile_tt']
-            next_day_tt = emergence_exceedance.iloc[0]['juvenile_tt']
-
-            if abs(prev_day_tt - tt_emergence) <= abs(next_day_tt - tt_emergence):
-                end_of_juvenile_date = pd.to_datetime(juvenile.loc[previous_day_index, 'Date'])
-            else:
-                end_of_juvenile_date = pd.to_datetime(emergence_exceedance.iloc[0]['Date'])
-
-            end_of_juvenile_date_doy = end_of_juvenile_date.dayofyear
-            df.loc[df['Date'] >= end_of_juvenile_date, 'End_of_juvenile_date'] = end_of_juvenile_date_doy
-        else:
-            df['End_of_juvenile_date'] = None
-    else:
-        df['End_of_juvenile_date'] = None
-
-    return df
-
-def endjuv2floral(df, stage_div):
-    df = emerg2endjuv(df, stage_div)
-    floral = df.dropna(subset=['End_of_juvenile_date'])
-    first_index = floral['End_of_juvenile_date'].first_valid_index()
+def calculate_stage(df, stage_div, previous_stage_col, current_stage_tt, current_stage_col, temperature_col):
+    stage = df.dropna(subset=[previous_stage_col])
+    first_index = stage[previous_stage_col].first_valid_index()
 
     if first_index is not None:
-        floral = df.loc[first_index + 1:].copy()
-        floral['floral_tt'] = floral['delta_TT'].cumsum()
+        stage = df.loc[first_index + 1:].copy()
+        stage[f'{current_stage_col}_tt'] = stage[temperature_col].cumsum()
 
-        tt_end_of_juvenile = stage_div['tt_end_of_juvenile']
-        juvenile_exceedance = floral[floral['floral_tt'] >= tt_end_of_juvenile]
+        tt_threshold = stage_div[current_stage_tt]
+        stage_exceedance = stage[stage[f'{current_stage_col}_tt'] >= tt_threshold]
 
-        if not juvenile_exceedance.empty:
-            first_exceedance_index = juvenile_exceedance.index[0]
-            previous_day_index = first_exceedance_index - 1 if first_exceedance_index > floral.index[0] else first_exceedance_index
+        if not stage_exceedance.empty:
+            first_exceedance_index = stage_exceedance.index[0]
+            previous_day_index = first_exceedance_index - 1 if first_exceedance_index > stage.index[0] else first_exceedance_index
 
-            prev_day_tt = floral.loc[previous_day_index, 'floral_tt']
-            next_day_tt = juvenile_exceedance.iloc[0]['floral_tt']
+            prev_day_tt = stage.loc[previous_day_index, f'{current_stage_col}_tt']
+            next_day_tt = stage_exceedance.iloc[0][f'{current_stage_col}_tt']
 
-            if abs(prev_day_tt - tt_end_of_juvenile) <= abs(next_day_tt - tt_end_of_juvenile):
-                floral_initiation_date = pd.to_datetime(floral.loc[previous_day_index, 'Date'])
+            if abs(prev_day_tt - tt_threshold) <= abs(next_day_tt - tt_threshold):
+                stage_date = pd.to_datetime(stage.loc[previous_day_index, 'Date'])
             else:
-                floral_initiation_date = pd.to_datetime(juvenile_exceedance.iloc[0]['Date'])
+                stage_date = pd.to_datetime(stage_exceedance.iloc[0]['Date'])
 
-            floral_initiation_date_doy = floral_initiation_date.dayofyear
-            df.loc[df['Date'] >= floral_initiation_date, 'floral_initiation_date'] = floral_initiation_date_doy
+            stage_date_doy = stage_date.dayofyear
+            df.loc[df['Date'] >= stage_date, current_stage_col] = stage_date_doy
         else:
-            df['floral_initiation_date'] = None
+            df[current_stage_col] = None
     else:
-        df['floral_initiation_date'] = None
-
-    return df
-
-def floral2flowering(df, stage_div):
-    df = endjuv2floral(df, stage_div)
-    flowering = df.dropna(subset=['floral_initiation_date'])
-    first_index = flowering['floral_initiation_date'].first_valid_index()
-
-    if first_index is not None:
-        flowering = df.loc[first_index + 1:].copy()
-        flowering['flowering_tt'] = flowering['Crown temperature (T_c)'].cumsum()
-
-        tt_floral_initiation = stage_div['tt_floral_initiation']
-        floral_exceedance = flowering[flowering['flowering_tt'] >= tt_floral_initiation]
-
-        if not floral_exceedance.empty:
-            first_exceedance_index = floral_exceedance.index[0]
-            previous_day_index = first_exceedance_index - 1 if first_exceedance_index > flowering.index[0] else first_exceedance_index
-
-            prev_day_tt = flowering.loc[previous_day_index, 'flowering_tt']
-            next_day_tt = floral_exceedance.iloc[0]['flowering_tt']
-
-            if abs(prev_day_tt - tt_floral_initiation) <= abs(next_day_tt - tt_floral_initiation):
-                flowering_date = pd.to_datetime(flowering.loc[previous_day_index, 'Date'])
-            else:
-                flowering_date = pd.to_datetime(floral_exceedance.iloc[0]['Date'])
-
-            flowering_date_doy = flowering_date.dayofyear
-            df.loc[df['Date'] >= flowering_date, 'flowering_date'] = flowering_date_doy
-        else:
-            df['flowering_date'] = None
-    else:
-        df['flowering_date'] = None
-
-    return df
-
-def flowering2heading(df, stage_div):
-    df = floral2flowering(df, stage_div)
-    heading = df.dropna(subset=['flowering_date'])
-    first_index = heading['flowering_date'].first_valid_index()
-
-    if first_index is not None:
-        heading = df.loc[first_index + 1:].copy()
-        heading['heading_tt'] = heading['Crown temperature (T_c)'].cumsum()
-
-        tt_flowering = stage_div['tt_flowering']
-        flowering_exceedance = heading[heading['heading_tt'] >= tt_flowering]
-
-        if not flowering_exceedance.empty:
-            first_exceedance_index = flowering_exceedance.index[0]
-            previous_day_index = first_exceedance_index - 1 if first_exceedance_index > heading.index[0] else first_exceedance_index
-
-            prev_day_tt = heading.loc[previous_day_index, 'heading_tt']
-            next_day_tt = flowering_exceedance.iloc[0]['heading_tt']
-
-            if abs(prev_day_tt - tt_flowering) <= abs(next_day_tt - tt_flowering):
-                heading_date = pd.to_datetime(heading.loc[previous_day_index, 'Date'])
-            else:
-                heading_date = pd.to_datetime(flowering_exceedance.iloc[0]['Date'])
-
-            heading_date_doy = heading_date.dayofyear
-            df.loc[df['Date'] >= heading_date, 'heading_date'] = heading_date_doy
-        else:
-            df['heading_date'] = None
-    else:
-        df['heading_date'] = None
-
-    return df
-
-def heading2endgf(df, stage_div):
-    df = flowering2heading(df, stage_div)
-    end_grain_fill = df.dropna(subset=['heading_date'])
-    first_index = end_grain_fill['heading_date'].first_valid_index()
-
-    if first_index is not None:
-        end_grain_fill = df.loc[first_index + 1:].copy()
-        end_grain_fill['end_grain_fill_tt'] = end_grain_fill['Crown temperature (T_c)'].cumsum()
-
-        tt_start_grain_fill = stage_div['tt_start_grain_fill']
-        heading_exceedance = end_grain_fill[end_grain_fill['end_grain_fill_tt'] >= tt_start_grain_fill]
-
-        if not heading_exceedance.empty:
-            first_exceedance_index = heading_exceedance.index[0]
-            previous_day_index = first_exceedance_index - 1 if first_exceedance_index > end_grain_fill.index[0] else first_exceedance_index
-
-            prev_day_tt = end_grain_fill.loc[previous_day_index, 'end_grain_fill_tt']
-            next_day_tt = heading_exceedance.iloc[0]['end_grain_fill_tt']
-
-            if abs(prev_day_tt - tt_start_grain_fill) <= abs(next_day_tt - tt_start_grain_fill):
-                end_grain_fill_date = pd.to_datetime(end_grain_fill.loc[previous_day_index, 'Date'])
-            else:
-                end_grain_fill_date = pd.to_datetime(heading_exceedance.iloc[0]['Date'])
-
-            end_grain_fill_date_doy = end_grain_fill_date.dayofyear
-            df.loc[df['Date'] >= end_grain_fill_date, 'end_grain_fill_date'] = end_grain_fill_date_doy
-        else:
-            df['end_grain_fill_date'] = None
-    else:
-        df['end_grain_fill_date'] = None
-
-    return df
-
-def endgf2maturity(df, stage_div):
-    df = heading2endgf(df, stage_div)
-    maturity = df.dropna(subset=['end_grain_fill_date'])
-    first_index = maturity['end_grain_fill_date'].first_valid_index()
-
-    if first_index is not None:
-        maturity = df.loc[first_index + 1:].copy()
-        maturity['maturity_tt'] = maturity['Crown temperature (T_c)'].cumsum()
-
-        tt_end_grain_fill = stage_div['tt_end_grain_fill']
-        end_grain_fill_exceedance = maturity[maturity['maturity_tt'] >= tt_end_grain_fill]
-
-        if not end_grain_fill_exceedance.empty:
-            first_exceedance_index = end_grain_fill_exceedance.index[0]
-            previous_day_index = first_exceedance_index - 1 if first_exceedance_index > maturity.index[0] else first_exceedance_index
-
-            prev_day_tt = maturity.loc[previous_day_index, 'maturity_tt']
-            next_day_tt = end_grain_fill_exceedance.iloc[0]['maturity_tt']
-
-            if abs(prev_day_tt - tt_end_grain_fill) <= abs(next_day_tt - tt_end_grain_fill):
-                maturity_date = pd.to_datetime(maturity.loc[previous_day_index, 'Date'])
-            else:
-                maturity_date = pd.to_datetime(end_grain_fill_exceedance.iloc[0]['Date'])
-
-            maturity_date_doy = maturity_date.dayofyear
-            df.loc[df['Date'] >= maturity_date, 'maturity_date'] = maturity_date_doy
-        else:
-            df['maturity_date'] = None
-    else:
-        df['maturity_date'] = None
+        df[current_stage_col] = None
 
     return df
 
 def wheat_stage_process(df, stage_div):
-    df = endgf2maturity(df, stage_div)
+    df = calculate_stage(df, stage_div, 'Emergence_date', 'tt_emergence', 'End_of_juvenile_date', 'delta_TT')
+    df = calculate_stage(df, stage_div, 'End_of_juvenile_date', 'tt_end_of_juvenile', 'floral_initiation_date', 'delta_TT')
+    df = calculate_stage(df, stage_div, 'floral_initiation_date', 'tt_floral_initiation', 'flowering_date', 'Crown temperature (T_c)')
+    df = calculate_stage(df, stage_div, 'flowering_date', 'tt_flowering', 'heading_date', 'Crown temperature (T_c)')
+    df = calculate_stage(df, stage_div, 'heading_date', 'tt_start_grain_fill', 'end_grain_fill_date', 'Crown temperature (T_c)')
+    df = calculate_stage(df, stage_div, 'end_grain_fill_date', 'tt_end_grain_fill', 'maturity_date', 'Crown temperature (T_c)')
+
     floral_initiation_date = df['floral_initiation_date'].dropna().unique()
     if len(floral_initiation_date) > 0:
         floral_initiation_date = \
@@ -219,7 +60,6 @@ def wheat_stage_process(df, stage_div):
         df = df[df['Date'] <= next_day]
 
     return df
-
 
 def main():
     latitude = 35.7281
